e41395559919257ccf4aab362d7d8038056f433e
Abdallah Wael <bedowael365@gmail.com>
2026-06-08 15:25:09 +0300

    chore: add missing newline at the end of providers.tsx file

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,6 +5,7 @@ import { HistoryProvider } from "@/app/modules/history/contexts/HistoryContext";
 import { LanguageProvider } from "@/app/modules/shared/contexts/LanguageContext";
 import { ToastProvider } from "@/app/modules/shared/components/Toast";
 
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <LanguageProvider>
